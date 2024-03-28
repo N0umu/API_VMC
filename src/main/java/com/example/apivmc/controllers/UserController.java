f3ba7621089e8bbe0f60cbb91141025894531b27
Lucas Labussiere <lucas.labussiere@uha.fr>
2024-03-28 11:32:17 +0100

    ajout de addBatimentToUserList

--- src/main/java/com/example/apivmc/controllers/UserController.java
+++ src/main/java/com/example/apivmc/controllers/UserController.java
@@ -1,7 +1,12 @@
 package com.example.apivmc.controllers;
 
+import com.example.apivmc.dao.ArchitecteDAO;
+import com.example.apivmc.dao.BatimentDAO;
+import com.example.apivmc.dao.CityDAO;
 import com.example.apivmc.dao.UserDAO;
+import com.example.apivmc.models.Architecte;
 import com.example.apivmc.models.Batiment;
+import com.example.apivmc.models.City;
 import com.example.apivmc.models.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,9 +19,15 @@ import java.util.Optional;
 @RequestMapping("/users")
 public class UserController {
     private UserDAO users;
+    private BatimentDAO batiments;
+    private CityDAO cities;
+    private ArchitecteDAO architectes;
 
-    public UserController(UserDAO users){
+    public UserController(UserDAO users, BatimentDAO batiments, CityDAO cities, ArchitecteDAO architectes){
         this.users = users;
+        this.batiments = batiments;
+        this.cities = cities;
+        this.architectes = architectes;
     }
 
     @GetMapping("")
@@ -52,16 +63,23 @@ public class UserController {
         return new ResponseEntity<>(HttpStatus.FORBIDDEN);
     }
 
-//    @PostMapping("/myList/{id}")
-//    public ResponseEntity<User> addBatimentToUserList(@PathVariable long id, @RequestBody BatimentDTO batimentInfo){
-//        Batiment batiment = new Batiment(batimentInfo.ville(), batimentInfo.nom(), )
-//        User created = new User(user.email(), user.password());
-//        if(this.findExistingUserWhereEmailLike(user.email()).isEmpty()){
-//            created = this.users.save(created);
-//            return new ResponseEntity<>(created, HttpStatus.OK);
-//        }
-//        return new ResponseEntity<>(HttpStatus.FORBIDDEN);
-//    }
+    @PostMapping("/myList/{id}")
+    public ResponseEntity<User> addBatimentToUserList(@PathVariable long id, @RequestBody BatimentDTO batimentInfo){
+        Optional<User> user = this.users.findById(id);
+        if (user.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+
+        Optional<Batiment> batiment = this.batiments.findExistingBatimentWhereLatAndLonLike(batimentInfo.lat(), batimentInfo.lon());
+        if (batiment.isEmpty()) {
+            Batiment newBatiment = createBatiment(batimentInfo);
+            user.get().add(newBatiment);
+        }else{
+            user.get().add(batiment.get());
+        }
+        this.users.save(user.get());
+        return new ResponseEntity<>(user.get(), HttpStatus.OK);
+    }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<User> deleteUserByID(@PathVariable int id) {
@@ -101,5 +119,31 @@ public class UserController {
     @GetMapping("/emailVerif/{email}")
     public Optional<User> findExistingUserWhereEmailLike(@PathVariable String email) {
         return this.users.findExistingUserWhereEmailLike(email);
+    }
+
+    public Batiment createBatiment(BatimentDTO batiment){
+
+        Batiment created = new Batiment(batiment.nom(), batiment.description(), batiment.adresse(), batiment.annee(), batiment.lat(), batiment.lon());
+        created = this.batiments.save(created);
+
+        Optional<Architecte> architecte = this.architectes.findExistingArchitecteWhereNomLike(batiment.archi());
+        if (architecte.isEmpty()) {
+            Architecte newArchi = new Architecte(batiment.archi());
+            newArchi.add(created);
+            this.architectes.save(newArchi);
+        }else{
+            architecte.get().add(created);
+            this.architectes.save(architecte.get());
+        }
+        Optional<City> city = this.cities.findExistingCityWhereNomLike(batiment.ville());
+        if (city.isEmpty()) {
+            City newCity = new City(batiment.ville());
+            newCity.add(created);
+            this.cities.save(newCity);
+        }else{
+            city.get().add(created);
+            this.cities.save(city.get());
+        }
+        return created;
     }
 }
--- src/main/java/com/example/apivmc/dao/BatimentDAO.java
+++ src/main/java/com/example/apivmc/dao/BatimentDAO.java
@@ -1,7 +1,10 @@
 package com.example.apivmc.dao;
 
 import com.example.apivmc.models.Batiment;
+import com.example.apivmc.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,6 +12,8 @@ import java.util.Optional;
 @Repository
 public interface BatimentDAO extends JpaRepository<Batiment, Long> {
 
+    @Query("select b from Batiment b where b.lat = :lat and b.lon = :lon")
+    Optional<Batiment> findExistingBatimentWhereLatAndLonLike(@Param("lat") String lat, @Param("lon") String lon);
     default Optional<Batiment> delete(long index) {
         Optional<Batiment> deleted = findById(index);
         if (deleted.isPresent()) {
